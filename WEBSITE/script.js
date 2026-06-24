90a00306e5dc0d9471ad0ed6ebf2ad80b80c1d39
rezaandrey1-eng <rezaandrey1@gmail.com>
2026-06-24 07:24:33 -0500

    Add files via upload

---
+++ WEBSITE/script.js
@@ -0,0 +1,31 @@
+function calculate() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+
+    let result;
+
+    if (operator === "+") {
+        result = num1 + num2;
+    }
+    else if (operator === "-") {
+        result = num1 - num2;
+    }
+    else if (operator === "*") {
+        result = num1 * num2;
+    }
+    else if (operator === "/") {
+
+        if (num2 === 0) {
+            document.getElementById("result").textContent =
+                "Result: Cannot divide by zero";
+            return;
+        }
+
+        result = num1 / num2;
+    }
+
+    document.getElementById("result").textContent =
+        "Result: " + result;
+}
